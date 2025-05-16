17bb1a20f922d653d476b5ef76e7139bb34704d7
Rayyan <rayyanraza91@gmail.com>
2025-05-16 10:12:46 +0500

    added user & video models

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ app.use(express.json({
     limit: "16kb"
 }))
 app.use(express.urlencoded({
-    extended: true
+    extended: true,
+    limit: "16kb"
 }))
 app.use(express.static("public"))
 app.use(cookieParser())
--- src/db/index.js
+++ src/db/index.js
@@ -13,3 +13,8 @@ const connectDB = async ()=>{
 }
 
 export default connectDB
+
+
+
+
+
