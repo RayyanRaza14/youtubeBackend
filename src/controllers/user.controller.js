934083e2014569309dd68b4a605b0334ce76ad61
Rayyan <rayyanraza91@gmail.com>
2025-05-20 11:53:57 +0500

    add user register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,65 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 
 const registerUser = asyncHandler(async (req, res)=> {
-    res.status(200).json({
-        message: "ok"
-    })
+   const {username, email, fullname, password} = req.body
+   console.log(username, fullname)
+   if(
+    [username, email, fullname, password].some((field)=> field?.trim() === "" )
+   ){
+    throw new ApiError(400, "All fields are necessary")
+   }
+
+   const existedUser = User.findOne({
+    $or: [{ username },{ email }]
+   })
+   if(existedUser){
+    throw new ApiError(409, "username and email already exist!")
+   }
+
+   const avatarLocalPath  = req.files?.avatar[0]?.path
+   const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+   if(!avatarLocalPath){
+    throw new ApiError(408, "Avatar file is required")
+   }
+
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!avatar){
+    throw new ApiError(407, "Avatar is missing ")
+   }
+
+   const user = await User.create({
+    username: username.toLowerCase(),
+    email,
+    password,
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+   })
+
+   const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+   )
+
+   if (!createdUser){
+    throw new ApiError(500, "User not created")
+   }
+
+   return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered Successfully")
+   )
+
+
+   
+
+
 })
 
 export {registerUser}
